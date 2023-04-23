70091ccedfbaaee6da7690ef2c70456768381501
RishitaChugh <117473767+RishitaChugh@users.noreply.github.com>
2023-04-23 13:33:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './App.css';
+import WrappedListComponent from './WrappedListComponent';
 
+const items = [
+  {
+    "text" : "rishita",
+  },
+  {
+    "text" : "12",
+  },
+  {
+    "text" : "NT"
+  },
+
+]
 const App = () => {
   return (
-    <div>App</div>
+   <>
+    <WrappedListComponent items = {items} />
+   </>
   )
 }
 
